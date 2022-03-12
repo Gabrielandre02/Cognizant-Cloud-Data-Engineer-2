2b40b5cc43a06632b48a29c3541ce87542665d62
gabriel andre <andre-carmo02@hotmail.com>
2022-03-12 13:11:55 -0300

    Comparador de hashe

---
+++ Comparador de Hashes/Hashe.py
@@ -0,0 +1,19 @@
+import hashlib
+
+arquivo = "C:\\Users\\gabriel\\Desktop\\Curso\\bot camp 2\\CODIGOS GIT\\DIO-BOOTCAMP\\Comparador de Hashes\\a.txt"
+arquivo1 = "C:\\Users\\gabriel\\Desktop\\Curso\\bot camp 2\\CODIGOS GIT\\DIO-BOOTCAMP\\Comparador de Hashes\\b.txt"
+
+texto1 = 'texto 1'
+texto2 = 'texto 2'
+
+hash1 = hashlib.new('ripemd160')
+hash2 = hashlib.new('ripemd160')
+
+
+hash1.update(open(arquivo, 'rb').read())
+hash2.update(open(arquivo1, 'rb').read())
+
+if hash1.digest() != hash2.digest():
+    print(f'O arquivo: {texto1} é diferente do arquivo {texto2}')
+else:
+    print(f'O (arquivo: {texto1}), é igual ao : (arquivo {texto2})')
