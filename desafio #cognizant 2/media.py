16b18557bfbe9f3a41fc92ac74795e630ec4c04e
gabriel andre <andre-carmo02@hotmail.com>
2022-03-13 17:47:48 -0300

    DESAFIO

---
+++ desafio #cognizant 2/media.py
@@ -0,0 +1,8 @@
+A = float(input())
+B = float(input())
+
+#TODO: Complete os espaços em branco com as respectivas variáveis para o cálculo da média.
+media = (A*3.5 + B*7.5)/(3.5+7.5);
+
+#TODO: Complete com a variável que representa o resultado da média.
+print(f'MEDIA = {media:.5f}');
