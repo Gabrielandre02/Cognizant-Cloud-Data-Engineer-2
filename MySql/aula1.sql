2b52e1f9cd337c038b298eb4708317e7e2db4438
gabriel andre <andre-carmo02@hotmail.com>
2022-03-20 12:34:28 -0300

    tabelas novas

--- MySql/aula1.sql
+++ MySql/aula1.sql
@@ -4,5 +4,24 @@ CREATE TABLE pessoa (
     nascimento DATE
 )
 
-INSERT INTO pessoa (nome, nascimento) VALUES ('Gabriel', '1997-11-02)
-INSERT INTO pessoa (nome, nascimento) VALUES ('Andre', '1997-02-11')
+INSERT INTO pessoa (nome, nascimento) VALUES ('Gabriel', '1997-11-02');
+
+UPDATE pessoa SET nome='Gabriel andre' WHERE id=1;
+
+DELETE FROM pessoa WHERE id=2;
+
+SELECT * FROM pessoa ORDER BY nome;
+
+SELECT * FROM pessoa ORDER BY nome DESC;
+
+UPDATE pessoa SET GENERO='M' WHERE id=4;
+UPDATE pessoa SET GENERO='M' WHERE id=6;
+
+ALTER TABLE `pessoa` CHANGE `GENERO` `genero` VARCHAR(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+SELECT COUNT(id), genero FROM pessoa GROUP BY genero;
+
+use teste;
+SHOW DATABASE;
+DROP DATABASE test;
+DROP TABLE usuarios;
